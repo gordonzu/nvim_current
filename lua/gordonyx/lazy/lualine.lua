93b78e9560f7eeea4053159ea4e196e07ff2d24f
gordonzu <gordonzu@outlook.com>
2025-03-18 17:32:41 -0400

    fix line config

--- lua/gordonyx/lazy/lualine.lua
+++ lua/gordonyx/lazy/lualine.lua
@@ -1,53 +1,48 @@
---if true then return {} end
-
-
-local everforest = require'lualine.themes.everforest'
+local everforest = require('lualine.themes.everforest')
 
 return {
 	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		event = "VeryLazy",
-		require('lualine').setup {
-			options = {
-				theme = everforest,
-				section_separators = '',
-				component_separator = '',
-			},
-			sections = {
-				lualine_a = {
-					function()
-						local mode_map = {
-							['n'] = 'N',
-							['i'] = 'I',
-							['v'] = 'V',
-							['V'] = 'VL',
-							[''] = 'VB', -- Visual Block
-							['c'] = 'C',
-							['s'] = 'S',
-							['S'] = 'SL',
-							[''] = 'SB', -- Select Block
-							['R'] = 'R',
-							['r'] = 'r',
-							['!'] = 'SH',
-							['t'] = 'T',
-						}
-						return mode_map[vim.api.nvim_get_mode().mode] or 'UNK'
-					end
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		event = 'VeryLazy',
+		config = function()
+			require('lualine').setup({
+				options = {
+					theme = everforest,
+					section_separators = '',
+					component_separators = '',
 				},
-				lualine_b = {'branch', 'diff', 'diagnostics'},
-				lualine_c = {'filename'},
-				lualine_x = {'encoding', 'fileformat', 'filetype'},
-				lualine_y = {'progress'},
-				lualine_z = {'location'}
-			}
-		}
-	}
+				sections = {
+					lualine_a = {
+						function()
+							local mode_map = {
+								['n'] = 'N',
+								['i'] = 'I',
+								['v'] = 'V',
+								['V'] = 'VL',
+								[''] = 'VB', -- Visual Block
+								['c'] = 'C',
+								['s'] = 'S',
+								['S'] = 'SL',
+								[''] = 'SB', -- Select Block
+								['R'] = 'R',
+								['r'] = 'r',
+								['!'] = 'SH',
+								['t'] = 'T',
+							}
+							return mode_map[vim.api.nvim_get_mode().mode] or 'UNK' 
+						end,
+					},
+					lualine_b = {'branch', 'diff', 'diagnostics'},
+					lualine_c = {'filename'},
+					lualine_x = {'encoding', 'fileformat', 'filetype'},
+					lualine_y = {'progress'},
+					lualine_z = {'location'}
+				},
+			})
+		end,
+	},
 }
-
-
-
-
 
 
 
